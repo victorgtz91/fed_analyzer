5e949ee7ccf9bcb69b3fd24f65e389736c35c1d3
victorgtz91 <134213968+victorgtz91@users.noreply.github.com>
2023-07-31 11:21:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,20 @@ st.title("Welcome, this is the Fed Analyzer")
 if st.button("RUN"):
 
     # Get the list of English stopwords
-    stop_words = set(stopwords.words('english'))
+    stop_words = set([
+        "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours",
+        "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers",
+        "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves",
+        "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are",
+        "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does",
+        "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until",
+        "while", "of", "at", "by", "for", "with", "about", "against", "between", "into",
+        "through", "during", "before", "after", "above", "below", "to", "from", "up", "down",
+        "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here",
+        "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more",
+        "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so",
+        "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"
+    ])
 
     # Add to the list of stop words
     stop_words.update(["federal", "reserve", "financial", "data", "system", "board", "page", "committee"])
